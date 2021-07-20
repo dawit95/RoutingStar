43f6ef68ce4d90b1451516172c5d4995f7457bf8
hyunohenn <hyun.ohenn@gmail.com>
2021-07-20 17:46:44 +0900

    FE_0720:store에 index.js에 모듈 추가

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import accounts from './modules/accounts.js'
+import routes from './modules/routes.js'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  // state: {
+  // },
+  // mutations: {
+  // },
+  // actions: {
+  // },
   modules: {
+    accounts,
+    routes,
   }
 })
