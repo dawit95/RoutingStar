f095321578c0f96c81cc5eadcf4303eca5861c62
edgar <edkim3275@gmail.com>
2021-07-20 17:50:06 +0900

    FE_0720:로그인 후 페이지 변경

--- frontend/src/components/accounts/Kakao.vue
+++ frontend/src/components/accounts/Kakao.vue
@@ -7,54 +7,75 @@
 </template>
 
 <script>
+// import axios from 'axios'
+
 export default {
   name: 'Kakao',
+  data: function () {
+    return {
+      isLogin: false
+    }
+  },
+  watch: {
+    isLogin () {
+      this.$router.push({ name: 'About'})
+    }
+  },
   methods: {
     kakaoLogin() {
       // 2. 인증코드로 토큰요청
       window.Kakao.Auth.login({
         scope: 'profile_nickname, account_email, gender',
         success: function (authObj) {
-
-          console.log(authObj);
+          // console.log(authObj);
+          const token = authObj.access_token
+          if (token) {
+            localStorage.setItem("access-token", token)
+          }
           // 3. 토큰으로  API 호출
           window.Kakao.API.request({
             url:'/v2/user/me',
             success: res => {
-              const kakao_account = res.kakao_account;
               console.log(res)
-              console.log(kakao_account)
-            }
+              // console.log(res.kakao_account);
+              // const kakao_account = res.kakao_account;
+              // const userInfo = {
+              //   nickname : kakao_account.profile.nickname,
+              //   email : kakao_account.email,
+              //   password : '',
+              //   account_type : 1,
+              }
+            //   axios({
+            //     method: 'post',
+            //     url: `https://server_url/api/user/login/kakao`,
+            //     data: {
+            //       userId : userInfo.email,
+            //       userName : userInfo.nickname,
+            //     }
+            //   })
+            //   .then(res => {
+            //     const token = res.data['auth_token'];
+            //     if (token) {
+            //       localStorage.setItem("access-token", token)
+            //       this.router.push("/home")
+            //     }
+            //   })
+            //   .catch(err => console.error(err))
+            // },
+            // fail : error => {
+            //     console.error(error);
+            // }
           });
+          // this.$router.push({ name: 'About'})
         }
       });
+      const token = localStorage.getItem('access-token')
+      if (token) {
+        this.$router.push("/about")
+      }
     }
   }
 }
-// export default {
-//   name: 'Kakao',
-//   methods: {
-//     kakaoLogin() {
-//       window.Kakao.Auth.login({
-//         scope: 'profile_nickname, account_email, gender',
-//         success: this.getMe,
-//       })
-//     },
-//     getMe(authObj) {
-//       console.log(authObj);
-//       window.Kakao.API.request({
-//         url: '/v2/user/me',
-//         success: res => {
-//           const kakao_account = res.kakao_account;
-//           console.log(kakao_account)
-//         },
-//         fail : error => {
-//             console.error(error);
-//         },
-//       });
-//     },
-//   },
-// }
 </script>
 
 <style>
