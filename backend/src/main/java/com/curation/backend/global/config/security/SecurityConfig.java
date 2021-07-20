c6766222b21c0a81efce71e0481022f2ca8b1a96
12dbwls34 <12dbwls34@naver.com>
2021-07-20 15:40:12 +0900

    BE_0720:user table 설계 및 SecurityConfig 설정
    h2를 이용하여 user table 설계함
    SecurityConfig로 h2-console/ 정적 리소스/ swagger 에 사용되는 url 접근 허용
    
    
    Former-commit-id: 53622009b2652b913ee91e33f6abdd92304858d1

---
+++ backend/src/main/java/com/curation/backend/global/config/security/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.curation.backend.global.config.security;
+
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(                        "/h2-console/**"
+                ,"/favicon.ico", "/swagger-ui.html/**", "/configuration/**", "/swagger-resources/**", "/v2/api-docs","/webjars/**", "/webjars/springfox-swagger-ui/*.{js,css}");
+    }
+}
