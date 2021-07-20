f7cb5382e75abc93b4e1b2ef7060e02b0d5334ed
12dbwls34 <12dbwls34@naver.com>
2021-07-20 15:40:12 +0900

    BE_0720:user table 설계 및 SecurityConfig 설정
    h2를 이용하여 user table 설계함
    SecurityConfig로 h2-console/ 정적 리소스/ swagger 에 사용되는 url 접근 허용
    
    
    Former-commit-id: 96577aeb3865181ce576475c55ad3b3b257a00bc

--- backend/src/main/java/com/curation/backend/user/domain/User.java
+++ backend/src/main/java/com/curation/backend/user/domain/User.java
@@ -1,4 +1,33 @@
 package com.curation.backend.user.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(nullable = true)
+    private String profileImg;
+
+    @Column(nullable = true)
+    private String userDescription;
+
+    @Column(nullable = true)
+    private String fileName;
+
+
 }
