685fd4d625d6202d28eda3a33c79c45da3248e9d
dawit95 <dawit0310@gmail.com>
2021-07-20 18:08:18 +0900

    BE_0720:add SpringSecurity Config
    Former-commit-id: b40adb6985e05080262543f91e758882af628e17

--- backend/src/main/java/com/curation/backend/global/config/security/SecurityConfig.java
+++ backend/src/main/java/com/curation/backend/global/config/security/SecurityConfig.java
@@ -1,11 +1,37 @@
 package com.curation.backend.global.config.security;
 
+import com.curation.backend.global.config.auth.CustomOAuth2UserService;
+import com.curation.backend.user.domain.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                .anyRequest().authenticated()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
+    }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
