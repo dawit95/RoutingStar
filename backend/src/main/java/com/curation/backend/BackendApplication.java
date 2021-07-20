28f0c1fa52f91e484315e953e4996cab2549d732
dawit95 <dawit0310@gmail.com>
2021-07-20 15:03:31 +0900

    BE_0720:git flow test
    Former-commit-id: 8b9f5bf807b3b72de41c57199eb180d3eb9acf8b

--- backend/src/main/java/com/curation/backend/BackendApplication.java
+++ backend/src/main/java/com/curation/backend/BackendApplication.java
@@ -3,6 +3,9 @@ package com.curation.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * git flow test 주석
+ */
 @SpringBootApplication
 public class BackendApplication {
 
