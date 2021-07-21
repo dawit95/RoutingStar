d52116b2e6533ba35f17cc99428cae6f17030413
edgar <edkim3275@gmail.com>
2021-07-21 16:11:31 +0900

    FE_0721: 메인페이지 bottom nav 추가

---
+++ frontend/src/components/common/Nav.vue
@@ -0,0 +1,50 @@
+<template>
+<v-card>
+    <v-bottom-navigation
+    absolute
+    :value="activeBtn"
+    grow
+    color="teal"
+  >
+    <v-btn>
+      <span>Search</span>
+      <v-icon>mdi-magnify</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Post</span>
+      <v-icon>mdi-star-circle-outline</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Home</span>
+      <v-icon>mdi-home</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+
+    <v-sheet
+      id="scroll-area-2"
+      class="overflow-y-auto"
+      max-height="605"
+    >
+      <v-container style="height: 800px;">
+
+      </v-container>
+    </v-sheet>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+  data () {
+    return {
+      bottomNav: 3,
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
