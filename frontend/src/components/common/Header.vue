a38763aa4557337c8664a6867d9e4148316278d3
fromecha <fromecha@gmail.com>
2021-07-21 15:48:51 +0900

    header vuetify

---
+++ frontend/src/components/common/Header.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-card class="overflow-hidden">
+    <v-app-bar
+      app
+      color="#fcb69f"
+      dark
+      src="https://picsum.photos/1920/1080?random"
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+        ></v-img>
+      </template>
+
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-app-bar-title>LOGO</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-bell</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-account</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-sheet
+      id="scrolling-techniques-2"
+      class="overflow-y-auto"
+      max-height="600"
+    >
+      <v-container style="height: 1000px;"></v-container>
+    </v-sheet>
+  </v-card>
+</template>
