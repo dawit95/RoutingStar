f120dccb2bcd5cb08826d18a2d4bb1767696a579
edgar <edkim3275@gmail.com>
2021-07-21 17:28:14 +0900

    FE_0721: nav버튼 routing설정

---
+++ frontend/src/views/search/SearchView.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <h1>Search Page</h1>
+  </v-app>
+
+</template>
+
+<script>
+
+
+export default {
+  name: 'SearchView',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
