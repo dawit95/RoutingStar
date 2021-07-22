35426cd7ae042e2584080753d1d538b889b270a5
fromecha <fromecha@gmail.com>
2021-07-22 21:22:18 +0900

    FE_0722: Map Submit DB 확인작업

--- frontend/src/components/lines/Map.vue
+++ frontend/src/components/lines/Map.vue
@@ -35,13 +35,12 @@
               <v-btn>썸네일 활용유무(선택시 이미지 썸네일로 적용)</v-btn>
             </v-list-item-content>
           </v-list-item>
-
         </v-list>
-
+        <v-btn @click="createPointList">Submit</v-btn>
       </v-flex>
-
     </v-layout>
   </v-container>
+
 </template>
 
 <script>
@@ -167,10 +166,11 @@ export default {
     addPoint(event) {
       const path = this.polyLine.getPath();
       path.push( event.latLng );
-      console.log(event)
+      // console.log(event)
       // console.log( event.latLng.lat());
       new window.google.maps.Marker( { position:event.latLng, map:this.map});
       this.addPointItem(event)
+      // console.log(this.SearchWord)
     },
     addPointItem (event) {
       // console.log('작동함 ㅇㅇ')
@@ -182,6 +182,20 @@ export default {
         thumbnail : false,
       }
       this.pointList.push(newPoint)
+    },
+    createPointList () {
+      // const mapItem = {
+      //   SearchWord: this.SearchWord,
+      //   map: this.map
+      // }
+      console.log(this.SearchWord)
+      console.log(this.map)
+      console.log(this.polyLine)
+      console.log(this.lat)
+      console.log(this.lng)
+      console.log(this.image)
+      console.log(this.pointInfo)
+      console.log(this.pointList)
     },
   },
   mounted() {
