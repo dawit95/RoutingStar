1428a4743c7edbe356507d70b39deca30d1ae3f0
fromecha <fromecha@gmail.com>
2021-07-23 12:12:50 +0900

    FE_0723: conflict reresovled

--- frontend/src/components/lines/Map.vue
+++ frontend/src/components/lines/Map.vue
@@ -18,29 +18,6 @@
 
       <v-divider></v-divider>
 
-<<<<<<< HEAD
-      <v-flex xs12>
-        <v-list
-          outlined
-          v-for="pointItem in pointList"
-          :key="pointItem.id"
-        >
-          <v-list-item outlined ma-0 pa-0>
-            <v-list-item-content>
-              <v-file-input label="첨부파일"></v-file-input>
-              <v-textarea
-                label="장소에대한 짧은설명"
-                rows="1"
-                prepend-icon="mdi-comment"
-              ></v-textarea>
-              <v-btn>썸네일 활용유무(선택시 이미지 썸네일로 적용)</v-btn>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-        <v-btn @click="createPointList">Submit</v-btn>
-      </v-flex>
-=======
->>>>>>> c2834bf7db966fc836d787f8468f6531aeb91538
     </v-layout>
   </v-container>
 
