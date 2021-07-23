968405190f3242f0c62171940b9a3911f5140d1e
12dbwls34 <12dbwls34@naver.com>
2021-07-23 14:28:21 +0900

    BE_0723:swagger 설정 등록
    Former-commit-id: e6c198edd0b4197fe8eb258389477f263d94dd77

--- backend/src/main/java/com/curation/backend/user/web/UserController.java
+++ backend/src/main/java/com/curation/backend/user/web/UserController.java
@@ -2,8 +2,8 @@ package com.curation.backend.user.web;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RequestMapping("/api/v1/user")
 @RestController
 public class UserController {
-
 }
