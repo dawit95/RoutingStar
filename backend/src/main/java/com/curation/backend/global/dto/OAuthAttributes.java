6eca16a24641dea25c0de57c506e98506366b4e9
dawit95 <dawit0310@gmail.com>
2021-07-23 13:22:44 +0900

    BE_0723: check merge
    Former-commit-id: 1e2e47eb635caca45b69d449ba82b8dd52878505

--- backend/src/main/java/com/curation/backend/global/dto/OAuthAttributes.java
+++ backend/src/main/java/com/curation/backend/global/dto/OAuthAttributes.java
@@ -26,6 +26,14 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+//        if("naver".equals(registrationId)) {
+//            return ofNaver("id", attributes);
+//        }
+
+        if("kakao".equals(registrationId)){
+            return ofKakao("id", attributes);
+        }
+
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -34,6 +42,30 @@ public class OAuthAttributes {
                 .name((String) attributes.get("name"))
                 .email((String) attributes.get("email"))
                 .profileImg((String) attributes.get("picture"))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+//    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+//        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+//
+//        return OAuthAttributes.builder()
+//                .name((String) response.get("name"))
+//                .email((String) response.get("email"))
+//                .profileImg((String) response.get("profile_image"))
+//                .attributes(response)
+//                .nameAttributeKey(userNameAttributeName)
+//                .build();
+//    }
+
+    private static OAuthAttributes ofKakao(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String,Object> response = (Map<String, Object>)attributes.get("kakao_account");
+        Map<String, Object> profile = (Map<String, Object>) response.get("profile");
+        return OAuthAttributes.builder()
+                .name((String)profile.get("nickname"))
+                .email((String)response.get("email"))
+                .profileImg((String)profile.get("profile_image_url"))
                 .attributes(attributes)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
