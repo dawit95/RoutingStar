5606439a12f06ef33bc71e287ee7f5e33fa46d41
edgar <edkim3275@gmail.com>
2021-07-28 02:24:53 +0900

    FE_0728:Map컴포넌트에서 pk데이터추가, draggable구현, draggable임시 컴포넌트생성
    Former-commit-id: a03025045f26fdc518926691f783f7624988327e

---
+++ frontend/src/views/draggable.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-card>
+    <v-toolbar dense color="success" dark flat>
+      드래그 테스트
+    </v-toolbar>
+    <v-card-text>
+      <draggable v-model="headers">
+        <v-chip
+          v-for="item in headers" :key="item.value"
+          class="ma-1"
+          @click="item.selected = !item.selected"
+          draggable
+          >
+          <v-icon v-if="item.selected">mdi-check</v-icon>
+          {{item.text}}
+        </v-chip>
+      </draggable>
+      
+    </v-card-text>
+    <v-data-table
+    :headers="selectedHeaders"
+    :items="items"
+    >
+    </v-data-table>
+  </v-card>
+</template>
+
+<script>
+import draggable from 'vuedraggable'
+
+export default {
+  name: 'Draggable',
+  components: {
+      draggable
+  },
+  data () {
+    return {
+      headers: [
+        { value: 'no', text: '번호', selected: true},
+        { value: 'title', text: '제목', selected: true},
+        { value: 'createdAt', text: '생성일', selected: true},
+        { value: 'UpdatedAt', text: '수정일', selected: false},
+        { value: 'readCount', text: '읽음', selected: true},
+        { value: 'commentCount', text: '댓글', selected: false},
+      ],
+      items: []
+    }
+  },
+  computed: {
+    selectedHeaders () {
+      return this.headers.filter(v => v.selected)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
