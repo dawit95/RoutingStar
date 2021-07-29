35ef7bd7791dd7b61f33d0932efc4daec82d90e9
fromecha <fromecha@gmail.com>
2021-07-29 10:10:53 +0900

    FE_0729: profile info 완료
    Former-commit-id: 35aa29acf62575cc06e4213f129afc792b7e7163

--- frontend/src/components/myPage/ProfileInfo.vue
+++ frontend/src/components/myPage/ProfileInfo.vue
@@ -3,7 +3,7 @@
       <v-container>
     <!-- 프로필 이미지 / 게시물 / 팔로워 / 팔로우 -->
         <v-row>
-          <v-col>
+          <v-col md="3">
           <image-input v-model="avatar">
             <div slot="activator">
               <v-avatar size="150px" v-ripple v-if="!avatar" class="grey lighten-3 mb-3">
@@ -14,7 +14,6 @@
               </v-avatar>
             </div>
           </image-input>
-          <div class="mt-4 title">CHAN</div>
           <!-- 이미지 저장할 때 모션 및 메서드로 나중에 submit 으로 대체할 예정 -->
           <v-slide-x-transition>
             <div v-if="avatar && saved == false">
@@ -22,12 +21,28 @@
             </div>
           </v-slide-x-transition>
           </v-col>
-          <v-col>
-            <div class="mt-4 title">0</div>
-            <div class="mt-4 title">좋아요</div>
-          </v-col>
+          <v-col md="9">
+            <v-row class="d-flex justify-center">
+              <div>NICKNAME</div>
+            </v-row>
+          
+            <v-row class="d=flex justify-center">
+              <v-col md="6">
+                <div class="mt-4 title">0</div>
+                <div class="mt-4 title">좋아요</div>
+              </v-col>
+              <v-col>
+                <div class="mt-4 title">0</div>
+                <div class="mt-4 title">싫어요</div>
+              </v-col>
+            </v-row>
+          </v-col>          
+        </v-row>
 
-         </v-row>
+        <v-row class="d-flex justify-center">
+          <div class="mt-4 title">user description</div>
+        </v-row>
+
     </v-container>
   </v-app>
 </template>
