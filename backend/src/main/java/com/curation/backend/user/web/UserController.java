b2787d080bfd6d9bd519f8fc2ffa0215b7a055f1
dawit95 <dawit0310@gmail.com>
2021-07-29 11:38:52 +0900

    BE_0729:에러 수정
    Former-commit-id: d1929fe6bf4da9016dc066441696cf99f4030297

--- backend/src/main/java/com/curation/backend/user/web/UserController.java
+++ backend/src/main/java/com/curation/backend/user/web/UserController.java
@@ -1,14 +1,5 @@
 package com.curation.backend.user.web;
 
-import com.curation.backend.tag.domain.WhatTag;
-import com.curation.backend.tag.domain.WhatTagRepository;
-import com.curation.backend.tag.domain.WithTag;
-import com.curation.backend.tag.domain.WithTagRepository;
-import com.curation.backend.user.domain.*;
-import com.curation.backend.user.service.UserService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
