d84a57127bd84b129420d7350a412929b95aa935
edgar <edkim3275@gmail.com>
2021-07-30 00:29:56 +0900

    FE_0730: MapPointForm 컴포넌트 주석부분 삭제, 삭제버튼 생성
    Former-commit-id: def7bf9d24cf55c5b5b9e16deb471d0f074959ce

--- frontend/src/components/routes/MapPointForm.vue
+++ frontend/src/components/routes/MapPointForm.vue
@@ -7,14 +7,10 @@
         :key="pointItem.id"
       >
         <v-icon drak large right style="cursor: pointer;">mdi-drag-horizontal-variant</v-icon>
+        <v-icon right large style="cursor: pointer;" @click="deleteItem(pointItem)">mdi-alpha-x-circle-outline</v-icon>
         <v-list-item outlined ma-0 pa-0 @click="forcheck(pointItem)">
-        <!-- <v-list-item outlined ma-0 pa-0> -->
           <v-list-item-content>
-            <!-- <v-file-input :change="pointItem.image" label="첨부파일"></v-file-input> -->
-            <!-- <input @change="upload" multiple accept="image/*" type="file" id="file" class="inputfile" /> -->
-            <!-- <input @change="onFileSelected(pointItem)" accept="image/*" type="file"> -->
-            <input @change="onFileSelected2(pointItem)" accept="image/*" type="file">
-
+            <input @change="onFileSelected(pointItem)" accept="image/*" type="file">
             <v-textarea v-model="pointItem.content" @click="activePoint(pointItem)" @mouseout="stopPoint(pointItem)" label="장소에대한 짧은설명" rows="1" prepend-icon="mdi-comment"></v-textarea>
             <v-btn>썸네일 활용유무(선택시 이미지 썸네일로 적용)</v-btn>
           </v-list-item-content>
@@ -49,21 +45,7 @@ export default {
     stopPoint(item) {
       item.marker.setAnimation(null);
     },
-    // upload(e){
-    //   var file = e.target.files;
-    //   let url = URL.createObjectURL(file[0]);
-    //   this.image = url
-    // },
-
-    // onFileSelected(pointItem) {
-    //   // 이미지 파일업로드되면 FormData로 만들기
-    //   this.selectedFile = event.target.files[0]
-    //   const fd = new FormData()
-    //   fd.append('image', this.selectedFile, this.selectedFile.name)
-    //   const idx = this.$store.getters.pointedItems.findIndex((e) => e.lat == pointItem.lat && e.lng == pointItem.lng );
-    //   this.$store.getters.imgList[idx] = fd
-    // },
-    onFileSelected2(pointItem) {
+    onFileSelected(pointItem) {
       this.selectedFile = event.target.files[0]
       const idx = this.$store.getters.pointedItems.findIndex((e) => e.lat == pointItem.lat && e.lng == pointItem.lng );
       this.$store.getters.imgList[idx] = this.selectedFile
@@ -80,9 +62,6 @@ export default {
           files.append('files', image)
         }
       }
-      // const check = files.get('files')
-      // console.log(check)
-      // this.$store.getters.imgList[0] = check
       axios({
         method: 'post',
         url: 'http://192.168.1.214:9091/api/v1/img/place',
@@ -142,6 +121,9 @@ export default {
     },
     dataInitCheck() {
       console.log(this.$store.getters.pointedItems)
+    },
+    deleteItem(pointItem) {
+      console.log(pointItem)
     }
   },
   computed: {
