a55b4ecdece2a64a6c7cfb37f85c7d6b15b9a93d
edgar <edkim3275@gmail.com>
2021-07-29 12:52:43 +0900

    FE_0729: draggable이후 이미지 보내기, 저장 완료
    Former-commit-id: e3b6c36a78e4ef81b3454c7facaf131483eb0999

--- frontend/src/components/routes/MapPointForm.vue
+++ frontend/src/components/routes/MapPointForm.vue
@@ -8,6 +8,7 @@
       >
         <v-icon drak large right style="cursor: pointer;">mdi-drag-horizontal-variant</v-icon>
         <v-list-item outlined ma-0 pa-0 @click="forcheck(pointItem)">
+        <!-- <v-list-item outlined ma-0 pa-0> -->
           <v-list-item-content>
             <!-- <v-file-input :change="pointItem.image" label="첨부파일"></v-file-input> -->
             <!-- <input @change="upload" multiple accept="image/*" type="file" id="file" class="inputfile" /> -->
@@ -21,6 +22,7 @@
       </v-list>
     </draggable>
     <button @click="tmp_complete">확인용 버튼</button>
+    <button @click="dataInitCheck">데이터 저장됐는지 확인용 버튼</button>
   </v-flex>
 </template>
 
@@ -106,24 +108,27 @@ export default {
     // 완료버튼이 눌러진다면
     tmp_complete() {
       const ins = this.$store.getters.pointedItems.length
-      const files = new FormData()
       for (var x = 0; x < ins; x++) {
+        const files = new FormData()
         const pk = this.$store.getters.pointedItems[x].pk
+        console.log(pk)
         if (this.$store.getters.imgList[pk] === null) {
           continue
         } else {
           const image =  this.$store.getters.imgList[pk]
+          // console.log('-------')
+          // console.log(pk)
           files.append('files', image)
           axios({
             method: 'post',
-            url: 'http://192.168.1.214:9091/api/v1/img/place',
+            url:'http://192.168.1.214:8000/api/v1/img/place',
             data: files,
             headers: {'Content-Type': 'multipart/form-data'}
           })
           .then(res => {
             console.log('보내짐')
-            console.log(res.data.image)
-            this.$store.getters.pointedItems[x] = res.data.image
+            const responseData = res.data.successDto.success.image
+            this.$store.getters.pointedItems[pk].image = responseData
           })
           .catch(err => {
             console.log('안보내짐')
@@ -133,7 +138,10 @@ export default {
       }
     },
     forcheck(item) {
-      console.log(item.pk)
+      console.log(`${item.pk}번째로 생성된 마커의 pk`)
+    },
+    dataInitCheck() {
+      console.log(this.$store.getters.pointedItems)
     }
   },
   computed: {
