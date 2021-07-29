5c001d4a09fbab9cf68907ad1546757f01ae1714
dawit95 <dawit0310@gmail.com>
2021-07-29 13:58:29 +0900

    BE_0729:modify oauth2_security_redirect-uri
    Former-commit-id: de5929a5de87366739c63748d0ce3e827ac9d1e8

--- backend/src/main/java/com/curation/backend/global/config/security/SecurityConfig.java
+++ backend/src/main/java/com/curation/backend/global/config/security/SecurityConfig.java
@@ -7,8 +7,6 @@ import com.curation.backend.token.service.TokenService;
 import com.curation.backend.user.domain.Role;
 import com.curation.backend.user.domain.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
