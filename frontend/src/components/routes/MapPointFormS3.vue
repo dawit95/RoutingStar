4f93b6c5ee2b187311e590a7c4d8b6030b393c37
edgar <edkim3275@gmail.com>
2021-08-04 15:17:30 +0900

    FE_0804:이미지 프론트에서 S3업로드 및 임시저장완료
    Former-commit-id: 89bfdca4b1584c53f0de95ebf1370c791b671583

--- frontend/src/components/routes/MapPointFormS3.vue
+++ frontend/src/components/routes/MapPointFormS3.vue
@@ -4,7 +4,7 @@
       <v-list
         outlined
         v-for="(pointItem, idx) in pointedItems"
-        :key="idx"
+        :key="pointItem.pk"
       >
         <v-icon drak large right style="cursor: pointer;">mdi-drag-horizontal-variant</v-icon>
 
@@ -25,7 +25,7 @@
         </v-list-item>
       </v-list>
     </draggable>
-    <button @click="upload">확인용 버튼</button>
+    <v-btn @click="postPointImages">확인용 버튼</v-btn>
   </v-flex>
 </template>
 
@@ -50,14 +50,17 @@ export default {
       isthumbail: false,
       selectedFile: null,
       thumbnailTxt: '',
-      albumBucketName: "routingstar-photo-album",
-      bucketRegion: "ap-northeast-2",
-      IdentityPoolId: "ap-northeast-2:d4f3505d-434d-4a7c-abf5-20472e6b4676",
+      albumBucketName: 'routingstar-photo-album',
+      bucketRegion: 'ap-northeast-2',
+      IdentityPoolId: 'ap-northeast-2:65af3722-b840-4cce-8c5f-956fb7ed025e',
     }
   },
   computed: {
     ...mapGetters(['pointedItems', 'imgList', 'polyLine']),
   },
+  // created() {
+  //   this.getFiles()
+  // },
   methods: {
     // 일단 데이터의 조작만 있고, 모든 데이터의 input이 완료된 이후에 백앤드와 통신하니
     // 일단은 mutations함수를 사용하였습니다.
@@ -73,86 +76,111 @@ export default {
     stopPoint(item) {
       item.marker.location.setAnimation(null);
     },
-    // 첨부파일 업로드되면 지속 업로드
+    // 첨부파일 업로드되면 imgList에 지속 업로드
     // 핀이 등록되면 해당 핀에의해 만들어진 리스트 인덱스를 가져와서 해당 인덱스를 활용하여 imgList에 이미지 넣기
+    // 이렇게해야만 사용자가 draggable로 리스트 이동후 파일 첨부를 해도 imgList의 올바른 자리에 사진이 갱신됩니다.
     onFileSelected(pointItem) {
       this.selectedFile = event.target.files[0]
-      const idx = this.pointedItems.findIndex((e) => e.lat == pointItem.lat && e.lng == pointItem.lng );
-      this.imgList[idx] = this.selectedFile
+      this.imgList[pointItem.pk] = this.selectedFile
       pointItem.imageUpload = true
     },
-    upload() {
+    postPointImages() {
+      console.log(this.imgList)
+      const ins = this.pointedItems.length
+      for (var x = 0; x < ins; x++) {
+        console.log(x + '번째 인덱스의 pk')
+        const pointItemPk = this.pointedItems[x].pk
+        console.log(pointItemPk)
+        // 첨부파일이 없다면 pass
+        if (this.imgList[pointItemPk] === '') {
+          continue
+          // 첨부파일이 있다면 S3로 업로드 진행 및 응답 데이터를 서버로 보내는 이미지 리스트에 저장
+        } else {
+          console.log('오키오키요')
+          const image = this.imgList[pointItemPk]
+          // S3로 업로드 후 반환값(이미지URL)을 저장
+          this.upload(image, x)
+        }
+      }
+      setTimeout(()=>{console.log(this.pointedItems)}, 5000)
+      
+    },
+    upload(image, x) {
       AWS.config.update({
-      region: this.bucketRegion,
-      credentials: new AWS.CognitoIdentityCredentials({
-          IdentityPoolId: this.IdentityPoolId
+        region: this.bucketRegion,
+        credentials: new AWS.CognitoIdentityCredentials({
+          IdentityPoolId: this.IdentityPoolId,
         })
       });
 
-      // var s3 = new AWS.S3({
-      //   apiVersion: "2006-03-01",
-      //   params: { Bucket: this.albumBucketName }
-      // });
-      const albumName = this.albumBucketName
-      var albumPhotosKey = encodeURIComponent(albumName) + "/";
-
-      var upload = new AWS.S3.ManagedUpload({
+      var s3 = new AWS.S3({
         apiVersion: "2006-03-01",
-        params: {
-          Bucket: albumPhotosKey,
-          Key: this.selectedFile.name,
-          Body: this.selectedFile
-        }
+        params: { Bucket: this.albumBucketName }
       });
-      var promise = upload.promise();
 
-      promise.then(
-        function(data) {
-          console.log(data)
-          alert("Successfully uploaded photo.");
-        },
-        function(err) {
+      s3.upload({
+        Key: image.name,
+        Body: image,
+        ContentType: 'image/jpeg',
+        ACL: 'public-read'
+      }, (err, data) => {
+        if (err) {
           return alert("There was an error uploading your photo: ", err.message);
         }
-      );
-
-
-
-
-      // s3.upload({
-      //   // Bucket: this.albumBucketName,
-      //   Key: this.selectedFile.name,
-      //   Body: this.selectedFile,
-      //   ACL: 'public-read'
-      // }, (err, data) => {
-      //   if (err) {
-      //     console.log(err)
-      //     return alert("There was an error uploading your photo: ", err.message);
-      //   }
-      //   alert("Successfully uploaded photo.");
-      //   console.log(data)
-      // });
-
-      // var upload = new AWS.S3.ManagedUpload({
-      //   params: {
-      //     Bucket: this.albumBucketName,
-      //     Key: this.selectedFile.name,
-      //     Body: this.selectedFile
-      //   }
-      // });
-
-      // var promise = upload.promise();
-
-      // promise.then(
-      //   function(data) {
-      //     console.log(data)
-      //     alert("Successfully uploaded photo.");
-      //   },
-      //   function(err) {
-      //     return alert("There was an error uploading your photo: ", err.message);
-      //   }
-      // );
+        // alert("Successfully uploaded photo.");
+        // const idx = this.pointedItems.findIndex((e) => e.pk === pointItemPk)
+        // console.log(idx, pointItemPk)
+        this.pointedItems[x].image = data.Location
+        // this.getFiles()
+      })
     },
+    // getFiles() {
+    //   AWS.config.update({
+    //     region: this.bucketRegion,
+    //     credentials: new AWS.CognitoIdentityCredentials({
+    //       IdentityPoolId: this.IdentityPoolId,
+    //     })
+    //   });
+
+    //   var s3 = new AWS.S3({
+    //     apiVersion: "2006-03-01",
+    //     params: { Bucket: this.albumBucketName }
+    //   });
+
+    //   s3.listObjects({ Delimiter: "/" }, function(err, data) {
+    //     if (err) {
+    //       return alert("There was an error listing your albums: " + err.message); 
+    //     } else {
+    //       var href = this.request.httpRequest.endpoint
+    //       var Url = href.protocol + '//' + href.host + '/' + data.Contents[0].Key
+    //       console.log(Url)
+    //       console.log('----')
+    //       console.log(data)
+    //     }
+    //   });
+    // },
+    // deleteFile (key) {
+    //   AWS.config.update({
+    //     region: this.bucketRegion,
+    //     credentials: new AWS.CognitoIdentityCredentials({
+    //       IdentityPoolId: this.IdentityPoolId,
+    //     })
+    //   });
+
+    //   var s3 = new AWS.S3({
+    //     apiVersion: "2006-03-01",
+    //     params: { Bucket: this.albumBucketName }
+    //   });
+      
+    //   s3.deleteObject({ Key: key }, (err, data) => {
+    //     if (err) {
+    //       return alert("There was an error deleting your photo: ", err.message);
+    //     }
+    //     alert("Successfully deleted photo.");
+    //     console.log(data)
+    //     this.getFiles()
+    //   });
+    // },
     // deleteItem 함수는 삭제하고 각각 component에서 조건에 맞게 포인트를 삭제한 후에
     // 삭제된 pointedItems를 변수로 설정 후 mutation 함수로 state에 반영하는 것으로 바꿈
     removePoint(marker, idx) {
@@ -169,6 +197,7 @@ export default {
     refreshPolyline() {
       const path = this.polyLine.getPath();
       const pointedItems = this.pointedItems
+      console.log(pointedItems)
       path.clear();
       for( const point of pointedItems ) {
         path.push( new window.google.maps.LatLng( point.lat, point.lng));
