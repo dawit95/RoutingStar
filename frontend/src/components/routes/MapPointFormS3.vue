9d378a7f772689f8976790329150a27cabf5663c
edgar <edkim3275@gmail.com>
2021-08-04 17:47:23 +0900

    FE_0804:변수명 맞추기, 썸네일 업로드 작업중
    Former-commit-id: 041de89c754e48d05cc84bb9679455eeac3c872f

--- frontend/src/components/routes/MapPointFormS3.vue
+++ frontend/src/components/routes/MapPointFormS3.vue
@@ -3,24 +3,24 @@
     <draggable @update="onUpdated">
       <v-list
         outlined
-        v-for="(pointItem, idx) in pointedItems"
-        :key="pointItem.pk"
+        v-for="(place, idx) in places"
+        :key="place.createdOrder"
       >
         <v-icon drak large right style="cursor: pointer;">mdi-drag-horizontal-variant</v-icon>
 
-        <v-icon right large style="cursor: pointer;" @click="removePoint(pointItem.marker, idx)">mdi-alpha-x-circle-outline</v-icon>
-        {{pointItem.pk}}
-        <v-list-item outlined ma-0 pa-0 @click="forcheck(pointItem)">
+        <v-icon right large style="cursor: pointer;" @click="removePoint(place.marker, idx)">mdi-alpha-x-circle-outline</v-icon>
+        {{place.createdOrder}}
+        <v-list-item outlined ma-0 pa-0 @click="forcheck(place)">
           <v-list-item-content>
-            <input @change="onFileSelected(pointItem)" accept="image/*" type="file">
-            <v-textarea v-model="pointItem.content" @click="activePoint(pointItem)" @mouseout="stopPoint(pointItem)" label="장소에대한 짧은설명" rows="1" prepend-icon="mdi-comment"></v-textarea>
+            <input @change="onFileSelected(place)" accept="image/*" type="file">
+            <v-textarea v-model="place.content" @click="activePoint(place)" @mouseout="stopPoint(place)" label="장소에대한 짧은설명" rows="1" prepend-icon="mdi-comment"></v-textarea>
           </v-list-item-content>
           <v-switch
           id="thumbnail_switch"
-          @click="refreshThumbnailBtn(pointItem)"
-          :label="pointItem.thumbnail ? thumbnailTxt : ''" 
-          :disabled="(!pointItem.thumbnail && isthumbail) || !pointItem.imageUpload"
-          :v-model="pointItem.thumbnail"
+          @click="refreshThumbnailBtn(place)"
+          :label="place.isThumbnail ? thumbnailLabel : ''" 
+          :disabled="(!place.isThumbnail && isthumbail) || !place.imageUpload"
+          :v-model="place.isThumbnail"
           inset color="indigo darken-3"></v-switch>
         </v-list-item>
       </v-list>
@@ -46,17 +46,17 @@ export default {
   },
   data () {
     return {
-      // 썸네일이 골라졌는지 유무
+      // 현재 썸네일이 골라진 상태인지 유무
       isthumbail: false,
       selectedFile: null,
-      thumbnailTxt: '',
+      thumbnailLabel: '',
       albumBucketName: 'routingstar-photo-album',
       bucketRegion: 'ap-northeast-2',
       IdentityPoolId: 'ap-northeast-2:65af3722-b840-4cce-8c5f-956fb7ed025e',
     }
   },
   computed: {
-    ...mapGetters(['pointedItems', 'imgList', 'polyLine']),
+    ...mapGetters(['places', 'imgList', 'polyLine', 'thumbnailImage']),
   },
   // created() {
   //   this.getFiles()
@@ -64,32 +64,32 @@ export default {
   methods: {
     // 일단 데이터의 조작만 있고, 모든 데이터의 input이 완료된 이후에 백앤드와 통신하니
     // 일단은 mutations함수를 사용하였습니다.
-    ...mapMutations(['UPDATE_DRAGGERBLE_ITEMS','REFRESH_POINTED_ITEMS']),
-    ...mapActions(['complete', 'updateThumbnailImage']),
+    ...mapMutations(['UPDATE_DRAGGERBLE_ITEMS','REFRESH_PLACES']),
+    ...mapActions(['updateThumbnailImage']),
     onUpdated(event) {
       this.UPDATE_DRAGGERBLE_ITEMS(event);
       this.refreshPolyline();
     },
-    activePoint(item) {
-      item.marker.location.setAnimation(window.google.maps.Animation.BOUNCE);
+    activePoint(place) {
+      place.marker.location.setAnimation(window.google.maps.Animation.BOUNCE);
     },
-    stopPoint(item) {
-      item.marker.location.setAnimation(null);
+    stopPoint(place) {
+      place.marker.location.setAnimation(null);
     },
     // 첨부파일 업로드되면 imgList에 지속 업로드
     // 핀이 등록되면 해당 핀에의해 만들어진 리스트 인덱스를 가져와서 해당 인덱스를 활용하여 imgList에 이미지 넣기
     // 이렇게해야만 사용자가 draggable로 리스트 이동후 파일 첨부를 해도 imgList의 올바른 자리에 사진이 갱신됩니다.
-    onFileSelected(pointItem) {
+    onFileSelected(place) {
       this.selectedFile = event.target.files[0]
-      this.imgList[pointItem.pk] = this.selectedFile
-      pointItem.imageUpload = true
+      this.imgList[place.createdOrder] = this.selectedFile
+      place.imageUpload = true
     },
     postPointImages() {
       console.log(this.imgList)
-      const ins = this.pointedItems.length
+      const ins = this.places.length
       for (var x = 0; x < ins; x++) {
         console.log(x + '번째 인덱스의 pk')
-        const pointItemPk = this.pointedItems[x].pk
+        const pointItemPk = this.places[x].createdOrder
         console.log(pointItemPk)
         // 첨부파일이 없다면 pass
         if (this.imgList[pointItemPk] === '') {
@@ -99,13 +99,14 @@ export default {
           console.log('오키오키요')
           const image = this.imgList[pointItemPk]
           // S3로 업로드 후 반환값(이미지URL)을 저장
-          this.upload(image, x)
+          this.upload(image, x, false)
         }
       }
-      setTimeout(()=>{console.log(this.pointedItems)}, 5000)
-      
+      setTimeout(()=>{console.log(this.places)}, 5000)
     },
-    upload(image, x) {
+    // 완료버튼이 눌러진다면
+    // 임시로 여기다 두고, api 파일 소화 후 이동 예정 이동 후 PostRouteDetail과 연동
+    upload(image, x, thumbnail) {
       AWS.config.update({
         region: this.bucketRegion,
         credentials: new AWS.CognitoIdentityCredentials({
@@ -128,12 +129,17 @@ export default {
           return alert("There was an error uploading your photo: ", err.message);
         }
         // alert("Successfully uploaded photo.");
-        // const idx = this.pointedItems.findIndex((e) => e.pk === pointItemPk)
+        // const idx = this.places.findIndex((e) => e.pk === pointItemPk)
         // console.log(idx, pointItemPk)
-        this.pointedItems[x].image = data.Location
-        // this.getFiles()
+        if (thumbnail) {
+          this.$store.state.thumbnailImage = data.Location
+          console.log(this.$store.state.thumbnailImage)
+        } else {
+          this.places[x].placeImg = data.Location
+        }
       })
     },
+    // s3에서 파일 가져오기
     // getFiles() {
     //   AWS.config.update({
     //     region: this.bucketRegion,
@@ -159,6 +165,8 @@ export default {
     //     }
     //   });
     // },
+
+    // s3에서 파일 삭제
     // deleteFile (key) {
     //   AWS.config.update({
     //     region: this.bucketRegion,
@@ -181,84 +189,49 @@ export default {
     //     this.getFiles()
     //   });
     // },
+
     // deleteItem 함수는 삭제하고 각각 component에서 조건에 맞게 포인트를 삭제한 후에
-    // 삭제된 pointedItems를 변수로 설정 후 mutation 함수로 state에 반영하는 것으로 바꿈
+    // 삭제된 places를 변수로 설정 후 mutation 함수로 state에 반영하는 것으로 바꿈
     removePoint(marker, idx) {
       if ( idx != -1 ) {
         marker.location.setMap(null);
-        const oldItems = this.pointedItems
-        const newPointedItems = oldItems.filter((point) => {
-          return point.pk !== marker.pk
+        const oldItems = this.places
+        const newplaces = oldItems.filter((place) => {
+          return place.createdOrder !== marker.createdOrder
         })
-        this.REFRESH_POINTED_ITEMS(newPointedItems)
+        this.REFRESH_PLACES(newplaces)
         this.refreshPolyline();
       }
     },
     refreshPolyline() {
       const path = this.polyLine.getPath();
-      const pointedItems = this.pointedItems
-      console.log(pointedItems)
+      const places = this.places
+      console.log(places)
       path.clear();
-      for( const point of pointedItems ) {
+      for( const point of places ) {
         path.push( new window.google.maps.LatLng( point.lat, point.lng));
       }
     },
-    // 완료버튼이 눌러진다면
-    // 임시로 여기다 두고, api 파일 소화 후 이동 예정 이동 후 PostRouteDetail과 연동
-
-    // postPointImages() {
-    //   const ins = this.pointedItems.length
-    //   for (var x = 0; x < ins; x++) {
-    //     const files = new FormData()
-    //     const pk = this.pointedItems[x].pk
-    //     // console.log(pk)
-    //     if (this.imgList[pk] === null) {
-    //       continue
-    //     } else {
-    //       const image =  this.imgList[pk]
-    //       files.append('files', image)
-    //       axios({
-    //         method: 'post',
-    //         url:'http://192.168.1.214:8000/api/v1/img/place',
-    //         data: files,
-    //         headers: {'Content-Type': 'multipart/form-data'}
-    //       })
-    //       .then(res => {
-    //         console.log('보내짐')
-    //         const responseData = res.data.success.image
-    //         console.log(responseData)
-    //         this.pointedItems[pk].image = responseData
-    //       })
-    //       .catch(err => {
-    //         console.log('안보내짐')
-    //         console.log(err)
-    //       })
-    //     }
-    //   }
-    //   console.log(this.pointedItems)
-    // },
-    forcheck(item) {
-      console.log(`${item.pk}번째로 생성된 마커의 pk`)
+    
+    forcheck(place) {
+      console.log(`${place.createdOrder}번째로 생성된 마커의 pk`)
     },
-    refreshThumbnailBtn(pointItem) {
-      // 썸네일 설정, 취소
+    // 썸네일 설정, 취소
+    refreshThumbnailBtn(place) {
       // 썸네일인 경우 => 썸네일 취소
       if (this.isthumbail) {
         this.isthumbail = !this.isthumbail
-        pointItem.thumbnail = false
-        this.thumbnailTxt = ''
+        place.isThumbnail = false
+        this.thumbnailLabel = ''
       // 썸네일이 아닌경우 => 첨부파일이 등록되어있다면 => 썸네일로 지정
       } else if (this.isthumbail === false) {
-        const idx = this.pointedItems.findIndex((e) => e.lat == pointItem.lat && e.lng == pointItem.lng );
-        if (this.imgList[idx] !== '') {
-          this.isthumbail = !this.isthumbail
-          pointItem.thumbnail = true
-          this.thumbnailTxt = '썸네일 이미지가 등록되었습니다.'
-          this.updateThumbnailImage(this.imgList[idx])
-        } else if (this.imgList[idx] === '') {
-          alert('장소에대한 이미지를 추가해주세요!')
-          pointItem.thumbnail = false
-        }
+        // const idx = this.places.findIndex((e) => e.lat == place.lat && e.lng == place.lng );
+        this.isthumbail = !this.isthumbail
+        place.isThumbnail = true
+        this.thumbnailLabel = '썸네일 이미지가 등록되었습니다.'
+        // console.log(this.imgList[place.createdOrder])
+        this.upload(this.imgList[place.createdOrder], 0, true)
+        // this.updateThumbnailImage(this.imgList[idx])
       }
     }
   },
