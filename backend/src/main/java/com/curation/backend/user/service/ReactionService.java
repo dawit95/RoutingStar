ea3a9719f470946556ece8dee1f627b5773c3a3c
dawit95 <dawit0310@gmail.com>
2021-08-04 17:44:27 +0900

    BE_0804:modify 올바른 순서 follow
    Former-commit-id: 3e5a7b5f20a7c1d8a217a330e58a2a7af8cdff5d

--- backend/src/main/java/com/curation/backend/user/service/ReactionService.java
+++ backend/src/main/java/com/curation/backend/user/service/ReactionService.java
@@ -74,8 +74,8 @@ public class ReactionService {
         User user = userRepository.findById(userId).orElseThrow(() -> new NoUserException("해당하는 사용자가 없습니다."));
 
         //userID가 follower인 모든 테이블에서 followingID만 뽑아 담은 list.
-        List<FollowerResponseDto> followerList = followerFollowingRepository.findAllByFollower(user).stream().map(FollowerResponseDto::new).collect(Collectors.toList());;
-        List<FollowingResponseDto> followingList = followerFollowingRepository.findAllByFollowing(user).stream().map(FollowingResponseDto::new).collect(Collectors.toList());;
+        List<FollowingResponseDto> followingList = followerFollowingRepository.findAllByFollower(user).stream().map(FollowingResponseDto::new).collect(Collectors.toList());;
+        List<FollowerResponseDto> followerList = followerFollowingRepository.findAllByFollowing(user).stream().map(FollowerResponseDto::new).collect(Collectors.toList());;
 
         FFResponseDto ffResponseDto = new FFResponseDto(followerList,followingList);
         return ffResponseDto;
