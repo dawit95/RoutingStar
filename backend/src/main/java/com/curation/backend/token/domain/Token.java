66b3df9f8d2a86f264fc29130136a4e6217e523c
dawit95 <dawit0310@gmail.com>
2021-08-05 10:47:59 +0900

    BE_0805:modify TokenController
    Former-commit-id: a6c84128f09e4c9790bf57b36ab5108832287fca

--- backend/src/main/java/com/curation/backend/token/domain/Token.java
+++ backend/src/main/java/com/curation/backend/token/domain/Token.java
@@ -8,12 +8,12 @@ import lombok.ToString;
 @NoArgsConstructor
 @Getter
 public class Token {
-    private String token;
-    private String refreshToken;
+    private String access_token;
+    private String refresh_token;
 
-    public Token(String token, String refreshToken) {
-        this.token = token;
-        this.refreshToken = refreshToken;
+    public Token(String access_token, String refresh_token) {
+        this.access_token = access_token;
+        this.refresh_token = refresh_token;
     }
 }
 
