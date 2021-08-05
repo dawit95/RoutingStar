81c7b851354ab457c8c517ecab1ab2e15f6ba1fc
dawit95 <dawit0310@gmail.com>
2021-08-05 09:32:04 +0900

    BE_0805:add  ignore
    Former-commit-id: a6065d8fa9dd8af48c715bb072b3bcf28f9ea922

--- backend/src/main/java/com/curation/backend/global/TestController.java
+++ backend/src/main/java/com/curation/backend/global/TestController.java
@@ -8,13 +8,11 @@ import com.curation.backend.user.exception.NoUserException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin("*")
 @RequestMapping("/userTest")
 @RequiredArgsConstructor
 @RestController
