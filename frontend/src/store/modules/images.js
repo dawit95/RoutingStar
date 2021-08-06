798968437fd46ae9c21fbbf90697f13b70fd6da9
edgar <edkim3275@gmail.com>
2021-08-06 13:50:53 +0900

    FE_0806: 이미지 첨부 없이 등록가능
    Former-commit-id: db717219914e3bbf281ee803df2302a6b93492b5

--- frontend/src/store/modules/images.js
+++ frontend/src/store/modules/images.js
@@ -27,6 +27,7 @@ const mutations= {
   //   routes.state.places[data.x] = data.responseData
   // },
   UPDATE_ROUTE_IMG(state, routeImg) {
+    console.log(`여기는 MUTATION${routeImg}`)
     state.routeImg = routeImg
   },
   UPDATE_THUMBNAIL_IMAGE(state, thumbnailImage) {
@@ -77,7 +78,7 @@ const actions = {
       if (routes.state.imgList[pointItemPk] === '') {
         continue
         // 첨부파일이 있다면 S3로 업로드 진행 및 응답 데이터를 서버로 보내는 이미지 리스트에 저장
-      } else {
+      } else if (routes.state.imgList[pointItemPk] !== '') {
         const image = routes.state.imgList[pointItemPk]
         // S3로 업로드 후 반환값(이미지URL)을 저장
 
@@ -90,8 +91,11 @@ const actions = {
     // }, 3000);
   },
   upload(payload) {
-    console.log('썸네일 업로드 들어옴')
-    console.log(payload.image, payload.num, payload.bool)
+    // console.log('썸네일 업로드 들어옴')
+    // console.log(payload.image, payload.num, payload.bool)
+    if (payload.image === undefined) {
+      return
+    }
     // 이미지를 저장하고자하는 S3 버킷지정하기(지역, ID, 버킷이름)
     AWS.config.update({
       region: state.bucketRegion,
@@ -104,26 +108,25 @@ const actions = {
       apiVersion: "2006-03-01",
       params: { Bucket: state.albumBucketName }
     });
-
+    
+    // const photoKey = payload.image.name
+    const date = new Date().getTime();
+    console.log(payload.image)
     s3.upload({
-      Key: payload.image.name,
+      Key: `${date + payload.image.name}`,
       Body: payload.image,
-      ContentType: 'image/jpeg',
+      ContentType: payload.image.type,
       ACL: 'public-read'
     }, (err, data) => {
       if (err) {
         console.log(err)
         return alert("There was an error uploading your photo: ", err.message);
       }
-      if (payload.bool) {
-        // commit('UPDATE_THUMBNAIL_IMAGE', data.Location)
-        state.thumbnailImage = data.Locaiton
-      } else {
-        routes.state.places[payload.num].placeImg = data.Location
-      }
+      routes.state.places[payload.num].placeImg = data.Location
     })
   },
   updateRouteImg({ commit }, routeImg) {
+    console.log(`여기는 actions${routeImg}`)
     commit('UPDATE_ROUTE_IMG', routeImg)
   },
   // updateThumbnailImage({ commit }, thumbnailImg) {
