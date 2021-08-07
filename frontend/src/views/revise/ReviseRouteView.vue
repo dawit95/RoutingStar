1039032bd43f6dc5fe0b700749689627e8272bcf
edgar <edkim3275@gmail.com>
2021-08-07 11:58:04 +0900

    FE_0807:루트 수정페이지 생성 시 기존의 핀 생성
    Former-commit-id: 0d2c338971b40942b4a81287bd401e8116c94d35

---
+++ frontend/src/views/revise/ReviseRouteView.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-layout row swap>
+    <UpdateMap/>
+  </v-layout>
+</template>
+
+<script>
+import UpdateMap from '@/components/revise/UpdateMap.vue'
+
+export default {
+  name: 'ReviseRouteView',
+  components: {
+    UpdateMap
+  },
+}
+</script>
+
+<style>
+
+</style>
