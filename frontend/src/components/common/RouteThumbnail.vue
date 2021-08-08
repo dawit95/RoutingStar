9e0a29917e1e2ffc3a9f01caf71bd66da24b1b39
hyunohenn <hyun.ohenn@gmail.com>
2021-08-09 00:30:47 +0900

    FE_0809: RouteThumbnail 루트 이미지와 썸네일 이미지 합친 컴포넌트 구현
    Former-commit-id: 5062a49e8186106ac47b26192dd18718f18fc7ad

--- frontend/src/components/common/RouteThumbnail.vue
+++ frontend/src/components/common/RouteThumbnail.vue
@@ -1,15 +1,59 @@
 <template>
   <div>
     <!-- 루트랑 썸네일 합치는 컴포넌트 입니다 -->
+    <div class="container" @click="onClick(route)">
+      <div class="box">
+        <div class="thumbnail"><img src="@/assets/temp_thumbnail.jpg" alt=""></div>
+        <!-- <div class="thumbnail"><img :src="routeThumbnail" alt="thumnbnail"></div> -->
+        <div class="routeImg"><img src="@/assets/tmp_route_img.png" alt=""></div>
+        <!-- <div class="routeImg"><img :src="routeImg" alt="routeImage"></div> -->
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  name: 'RouteThumbnail',
+  props: {
+    // 어떤 루트인지는 상위 컴포넌트에서 지정된다
+    route: Object,
+  },
+  computed: {
+    // 썸네일 이미지 찾아내기
+    routeThumbnail () {
+      return this.route.find( e => e.isThumbnail === true ).placeImg
+    },
+    routeImg() {
+      return this.route.routeImg
+    }
+  }
 }
 </script>
 
 <style>
-
+.container {
+  margin: 0px;
+  padding: 0px;
+}
+img {
+  width: 60px; height: 60px;
+  object-fit: cover;
+  object-position: top;
+  border-radius: 50%;
+}
+.box {
+  position: relative;
+}
+.thumbnail {
+  top: 0;
+  left: 0;
+}
+.routeImg {
+  position: absolute;
+  top: 0;
+  left: 0;
+  transform: translate( 10%, 10% );
+}
 </style>
