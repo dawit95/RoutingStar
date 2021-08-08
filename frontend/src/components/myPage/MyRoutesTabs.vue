5ecad9a5d395fd82b894b76757c0c96edcc37834
hyunohenn <hyun.ohenn@gmail.com>
2021-08-09 01:06:29 +0900

    FE_0809: Thumbnail + routeImgg 마이페이지에 삽입
    Former-commit-id: 1f29d0fac3775152105800bb98bcad949a489b28

--- frontend/src/components/myPage/MyRoutesTabs.vue
+++ frontend/src/components/myPage/MyRoutesTabs.vue
@@ -15,6 +15,11 @@
               aria-hidden="false">
             <ul>
               <li>
+                <!-- <RouteThumbnail
+                  v-for="route in myRouteList"
+                  :key="route.id"
+                  :route="route"
+                /> -->
                 <RouteThumbnail/>
               </li>
             </ul>
@@ -33,7 +38,16 @@
               role="tabpanel" 
               aria-labelledby="specification" 
               aria-hidden="true">
-            <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla?</p>
+            <ul>
+              <li>
+                <!-- <RouteThumbnail
+                  v-for="route in followingRouteList"
+                  :key="route.id"
+                  :route="route"
+                /> -->
+                <RouteThumbnail/>
+              </li>
+            </ul>
           </div>
         </li>
     </ul>
@@ -42,12 +56,20 @@
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex'
 import RouteThumbnail from '@/components/common/RouteThumbnail.vue'
 
 export default {
   name: 'MyRoutesTabs',
-  components: {
-    RouteThumbnail
+  components: { RouteThumbnail },
+  computed: {
+    ...mapGetters(['myRouteList', 'followingRouteList'])
+  },
+  methods: {
+    ...mapActions([''])
+  },
+  mounted() {
+    
   }
 }
 </script>
--- frontend/src/views/myPage/OtherUserPageView.vue
+++ frontend/src/views/myPage/OtherUserPageView.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
-    <h1>로그인한 유저가 보는 남의 페이지입니다.</h1>
+    <p style="color: white">로그인한 유저가 보는 남의 페이지입니다.</p>
     <ProfileInfo/>
+    <div class="button mx-8">Edit Profile</div>
     <OtherUserRoutesTab/>
 
   </div>
@@ -9,17 +10,54 @@
 
 <script>
 import ProfileInfo from '@/components/myPage/ProfileInfo.vue'
-import OtherUserRoutesTab from '@/components/myPage/OtherUserRoutesTab.vue'
+import MyRoutesTabs from '@/components/myPage/MyRoutesTabs.vue'
 
 export default {
-  name: 'MyPage',
+  name: 'OtherUserPageView',
   components: {
     ProfileInfo,
-    OtherUserRoutesTab,
+    MyRoutesTabs,
   },
 }
 </script>
 
 <style>
-
+.button {
+  flex: 1 1 auto;
+  padding: 3px;
+  margin: 7px;
+  border: 2px solid #D2FDFF;
+  text-align: center;
+  text-transform: uppercase;
+  position: relative;
+  overflow: hidden;
+  transition: 0.3s;
+  color: #D2FDFF;
+  height: 30px;
+  font-size: 13px;
+  background-color: #101423;
+}
+.button:after {
+  position: absolute;
+  transition: 0.3s;
+  content: "";
+  width: 0;
+  left: 50%;
+  bottom: 0;
+  height: 3px;
+  background: #D2FDFF;
+}
+.button:hover {
+  cursor: pointer;
+}
+.button:hover:after {
+  width: 100%;
+  left: 0;
+}
+.button:hover {
+  box-shadow: 0px 0px 0px 3px #f7f7f7;
+}
+.button:hover:after {
+  width: 0%;
+}
 </style>
