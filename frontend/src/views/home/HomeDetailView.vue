5603595b7b42bea262354c6d387d71e54181baf6
fromecha <fromecha@gmail.com>
2021-08-09 17:06:54 +0900

    FE_0809: 메인페이지 및 프로필 썸네일 및 닉네임 클릭시 프로필 페이지로 이동
    Former-commit-id: 032de4afdca9325a755c2aa88317f8ff6fd7aad1

---
+++ frontend/src/views/home/HomeDetailView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{ feed }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeDetailView',
+  props: {
+    feed: {
+      type: Object,
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
