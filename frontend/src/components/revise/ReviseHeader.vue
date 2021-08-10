01ebbcdc638cc6d5a463f1e9d975a5bad337a752
edgar <edkim3275@gmail.com>
2021-08-11 01:38:02 +0900

    FE_0811: ReviseHeader icon으로 변경
    Former-commit-id: 60d27d1b33533f9b87b40881c355f4d8f8281133

--- frontend/src/components/revise/ReviseHeader.vue
+++ frontend/src/components/revise/ReviseHeader.vue
@@ -1,13 +1,42 @@
 <template>
-  
+    <v-card class="overflow-hidden">
+    <v-app-bar
+      app
+      color="#101423"
+      dark
+      style="width=100%; justify-content=flex-end;"
+      pa-0
+    >
+      <v-btn
+      @click="handleClick1"
+      icon>
+        <v-icon x-large>mdi-chevron-left</v-icon>
+      </v-btn>
+      <v-spacer></v-spacer>
+
+      <v-btn @click="handleClick2" color="#101423" right dark>선택완료</v-btn>
+    </v-app-bar>
+  </v-card>
 </template>
 
 <script>
 export default {
   name: 'ReviseHeader',
+  methods: {
+    handleClick1() {
+      console.log('hello1')
+    },
+    handleClick2() {
+      console.log('hello2')
+    },
+    
+  }
 }
 </script>
 
 <style>
-
+.v-toolbar__content, .v-toolbar__extension {
+  padding-right: 0px;
+  padding-left: 8px;
+}
 </style>
