aa692727c434a89d1b99d01f2b477294e4a68016
12dbwls34 <12dbwls34@naver.com>
2021-08-12 02:09:57 +0900

    BE_0812:deploy.sh 수정
    Former-commit-id: 122257cac7bcec48c44b181abffe2fd194fd66ab [formerly a44a7659d6e52295f601a0a07f0634a96592914f]
    Former-commit-id: f0172ebb8b214b1ab917577c3b6e30914d455532

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -2,4 +2,6 @@ echo "hello"
 cd /home/ubuntu/app/backend
 pwd
 docker build -t routingstar/backend:latest .
-docker run -d -p 8081:8081 routingstar/backend --name routingstar/backend
+docker container stop routingstar
+docker rm routingstar
+docker run -d --name routingstar -p 8081:8081 routingstar/backend
