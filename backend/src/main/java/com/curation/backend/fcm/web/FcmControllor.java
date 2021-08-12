53a5baba47757999aafa118ab2a692da4f63bf3b
dawit95 <dawit0310@gmail.com>
2021-08-12 11:21:35 +0900

    BE_0812:add FcmControllor logger
    Former-commit-id: a9ba196963de91fcd7ae568a4543c171ee0e9b76

--- backend/src/main/java/com/curation/backend/fcm/web/FcmControllor.java
+++ backend/src/main/java/com/curation/backend/fcm/web/FcmControllor.java
@@ -4,6 +4,8 @@ import com.curation.backend.fcm.service.FcmService;
 import com.curation.backend.global.dto.SuccessResponseDto;
 import com.curation.backend.global.service.ResponseGenerateService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,13 +25,16 @@ public class FcmControllor {
     private final ResponseGenerateService responseGenerateService;
     private final FcmService fcmService;
 
+    Logger logger = LoggerFactory.getLogger(FcmControllor.class);
+
     @GetMapping("{userId}")
     public ResponseEntity<SuccessResponseDto> setToken (@PathVariable("userId") Long userId,ServletRequest request, ServletResponse response) {
         String message = "";
 
         String browser_token = ((HttpServletRequest)request).getHeader("browser_token");
-
+        logger.trace("브라우저토큰 왔음 {} ",browser_token);
         message = fcmService.setBrowserToken(userId,browser_token);
+        logger.trace("브라우저토큰 저장 완료!!");
 
         HttpStatus status = HttpStatus.OK;
         SuccessResponseDto successResponseDto = responseGenerateService.generateSuccessResponse(message);
