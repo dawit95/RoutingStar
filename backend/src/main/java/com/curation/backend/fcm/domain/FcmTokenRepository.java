cb39ba948646880340be0b507d8893ab93f96877
dawit95 <dawit0310@gmail.com>
2021-08-12 17:09:30 +0900

    BE_0812: modify Alarm
    Former-commit-id: 2c12843e5515be9bd7bbcf851e3f2a0f775a2641

--- backend/src/main/java/com/curation/backend/fcm/domain/FcmTokenRepository.java
+++ backend/src/main/java/com/curation/backend/fcm/domain/FcmTokenRepository.java
@@ -4,12 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface FcmTokenRepository extends JpaRepository<FcmToken, Long> {
-    Optional<FcmToken> findbyUserId(Long userId);
+//    Optional<FcmToken> findFcmTokenByUserId(Long userId);
     List<FcmToken> findFcmTokenByUserId(Long userId);
-    Optional<FcmToken> deleteAllByUserId(Long userId);
+//    Optional<FcmToken> deleteAllByUserId(Long userId);
     boolean existsByUserId(Long userId);
 }
