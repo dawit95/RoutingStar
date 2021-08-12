527a45e17ad9eb2a351c2c79efad6631de1fc0b0
12dbwls34 <12dbwls34@naver.com>
2021-08-12 17:06:03 +0900

    FE_0812:도커 이미지 생성 테스트
    Former-commit-id: a4e6a4847600fa4f890079603d0c6f9856a929aa [formerly 6c1d79c1dbcaa28038bd1f01acea6b2e294f3495]
    Former-commit-id: 1408668736d08151c146ee93f77eb8bbf07d64d4

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,7 +1,7 @@
 echo "hello frontend"
 cd /home/ubuntu/dist
 pwd
-docker build -t routingstar/frontend:latest
+docker build -t routingstar/frontend:latest .
 docker container stop nginx
 docker rm nginx
 docker run -d --name nginx -p 80:80 routingstar/frontend
