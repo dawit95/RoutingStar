e85ba306a42d43f8596b7a0e897c6967d28d0286
12dbwls34 <12dbwls34@naver.com>
2021-08-12 14:49:14 +0900

    FE_0812:포트 번호 수정
    Former-commit-id: 99c096660ba55d94d5e8e92dacffcf9995050da6 [formerly aa3c107e792fd1850bf5968e16a5ed0ff3f8c65c]
    Former-commit-id: b95a1f9576d9bbcf15bf0ec6b0b8132f0c33bad7

--- frontend/src/components/accounts/Google.vue
+++ frontend/src/components/accounts/Google.vue
@@ -2,13 +2,13 @@
 <div>
     <!-- 수정 1. google 2. vuex accounts -->
     <!-- <a href="http://localhost:8000/oauth2/authorization/kakao"> -->
-    <a href="http://i5a309.p.ssafy.io/oauth2/authorization/kakao">
+    <a href="http://i5a309.p.ssafy.io:8081/oauth2/authorization/kakao">
 
       <v-img class="mx-auto logoTopM" max-height="300" max-width="300" :src="require('../../assets/kakao_login_medium_wide.png')" />
     </a>
     <div class="rounded-lg">
 
-    <a href="http://i5a309.p.ssafy.io/oauth2/authorization/google">
+    <a href="http://i5a309.p.ssafy.io:8081/oauth2/authorization/google">
       <v-img class="mx-auto loginButtonM circle" max-height="40" max-width="315" :src="require('../../assets/btn_google_signin_dark_pressed_web@2x.png')" />
     </a>
     </div>
