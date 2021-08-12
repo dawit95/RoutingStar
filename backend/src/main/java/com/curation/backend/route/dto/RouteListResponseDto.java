ab2c3b02c4638c554dc8a7602601a26abda3a33f
12dbwls34 <12dbwls34@naver.com>
2021-08-13 01:18:36 +0900

    BE_0813:필요없는 파일 제거 및 태그 추가
    Former-commit-id: d6f8fc40e652a2f7ea2f5db37e0515aadd4f7df0

--- backend/src/main/java/com/curation/backend/route/dto/RouteListResponseDto.java
+++ backend/src/main/java/com/curation/backend/route/dto/RouteListResponseDto.java
@@ -2,6 +2,8 @@ package com.curation.backend.route.dto;
 
 import com.curation.backend.place.dto.PlaceResponseDto;
 import com.curation.backend.route.domain.Route;
+import com.curation.backend.tag.dto.WhatTagResponseDto;
+import com.curation.backend.tag.dto.WithTagResponseDto;
 import com.curation.backend.user.dto.UserResponseDto;
 import lombok.Getter;
 import lombok.Setter;
@@ -19,6 +21,8 @@ public class RouteListResponseDto {
     private LocalDateTime modifiedAt;
     private UserResponseDto user;
     private List<PlaceResponseDto> places;
+    private List<WhatTagResponseDto> whatTag;
+    private List<WithTagResponseDto> withTag;
 
     private Long likeCnt;
     private Long storageCnt;
@@ -33,6 +37,9 @@ public class RouteListResponseDto {
         this.modifiedAt = entity.getModifiedAt();
         this.user = new UserResponseDto(entity.getUser());
         this.places = entity.getRoutePlaces().stream().map(PlaceResponseDto::new).collect(Collectors.toList());
+
+        this.whatTag = entity.getRouteWhatTags().stream().map(e -> new WhatTagResponseDto(e.getWhatTag())).collect(Collectors.toList());
+        this.withTag = entity.getRouteWithTags().stream().map(e -> new WithTagResponseDto(e.getWithTag())).collect(Collectors.toList());
 
         this.likeCnt = Long.valueOf(entity.getRouteLikes().size());
         this.storageCnt = Long.valueOf(entity.getRouteStorages().size());
