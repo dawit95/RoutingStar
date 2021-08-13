7757f811061d7c9e6722e9f0da0b111571b292f4
12dbwls34 <12dbwls34@naver.com>
2021-08-13 12:05:10 +0900

    BE_0813:백엔드 서버 CI 과정
    Former-commit-id: 8b1e91c0e3bf5ecb45cdbd352a608aefa7656758 [formerly b9148ba4fde2645564971b59d6cee7d904d67b9c]
    Former-commit-id: bbf89e29257332839cf82159a8a0b2e039d4d41d

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -4,4 +4,4 @@ pwd
 docker build -t routingstar/backend:latest .
 docker container stop routingstar
 docker rm routingstar
-docker run -d --name routingstar -p 8081:8081 routingstar/backend
+docker run -d --name routingstar -p 8000:8000 routingstar/backend
