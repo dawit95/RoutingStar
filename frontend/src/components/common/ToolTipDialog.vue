ece09e4e32dca1d03d5f5205220885d14eaa8acd
12dbwls34 <12dbwls34@naver.com>
2021-08-16 21:20:36 +0900

    FE_0816: 서비스 가이드 확인 중
    Former-commit-id: 9bfc977dc3eb9cfd1c028b96337966ad80ba0067

--- frontend/src/components/common/ToolTipDialog.vue
+++ frontend/src/components/common/ToolTipDialog.vue
@@ -12,7 +12,7 @@
       <v-card>
         <v-toolbar
           dark
-          color="primary"
+          color="#101423"
         >
           <v-btn
             icon
@@ -21,7 +21,7 @@
           >
             <v-icon>mdi-close</v-icon>
           </v-btn>
-          <v-toolbar-title>Settings</v-toolbar-title>
+          <v-toolbar-title>👏 User Guide</v-toolbar-title>
           <v-spacer></v-spacer>
         </v-toolbar>
         <user-guide></user-guide>
