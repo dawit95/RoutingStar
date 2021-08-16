49a31aa959fbf39f3c060fbb19daa0f2ce45a56b
12dbwls34 <12dbwls34@naver.com>
2021-08-16 21:35:54 +0900

    FE_0816: 로그인 api 수정
    Former-commit-id: 0a85146ab477692875e36826484782c07c62809c [formerly b26e74efbbdc3b07ec252e463befd08a37d795b3]
    Former-commit-id: 54f1208f8438d618f74e22cfa1d7b41a2db5f3bf

--- frontend/src/components/accounts/Google.vue
+++ frontend/src/components/accounts/Google.vue
@@ -3,12 +3,12 @@
     <!-- 수정 1. google 2. vuex accounts -->
     <!-- <a href="http://localhost:8000/oauth2/authorization/kakao"> -->
     <!-- <h1 style="color: white">하이하이</h1> -->
-    <a href="https://i5a309.p.ssafy.io/oauth2/authorization/kakao">
+    <a href="http://i5a309.p.ssafy.io:8000/oauth2/authorization/kakao">
       <v-img class="mx-auto logoTopM" max-height="300" max-width="300" :src="require('../../assets/kakao_login_medium_wide.png')" />
     </a>
     <div class="rounded-lg">
 
-    <a href="https://i5a309.p.ssafy.io/oauth2/authorization/google">
+    <a href="http://i5a309.p.ssafy.io:8000/oauth2/authorization/google">
       <v-img class="mx-auto loginButtonM circle" max-height="46" max-width="308" :src="require('../../assets/btn_google_signin_light_focus_web@2x.png')" />
     </a>
     </div>
