8a1a274e714a4c59e8ba40c6527ace6ac6484b80
dawit95 <dawit0310@gmail.com>
2021-08-17 00:50:16 +0900

    FE_0817: 사진 업로드 속성 추가 accept=images/*
    Former-commit-id: 66b96872cde86b856cd7ead311e958517e999214 [formerly ebf146d86fd9966887a6906e8009fa39d56077d7]
    Former-commit-id: 9fff9fecd9d0907d11bee2da9dbe4d5464c6b703

--- frontend/src/components/routes/PostImageInput.vue
+++ frontend/src/components/routes/PostImageInput.vue
@@ -10,94 +10,105 @@
       <slot name="activator"></slot>
     </div>
     <!-- <input type="file" ref="file" :name="uploadFieldName" @change="onFileChange($event.target.name, $event.target.files)" style="display:none"> -->
-    <input capture type="file" ref="file" :name="uploadFieldName" @change="onFileSelected(place, $event.target.name, $event.target.files)" style="display:none">
+    <input
+      capture
+      type="file"
+      accept="images/*"
+      ref="file"
+      :name="uploadFieldName"
+      @change="onFileSelected(place, $event.target.name, $event.target.files)"
+      style="display:none"
+    />
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import AWS from 'aws-sdk'
+import { mapGetters } from 'vuex';
+import AWS from 'aws-sdk';
 
 export default {
   name: 'PostImageInput',
-  data: ()=> ({
+  data: () => ({
     uploadFieldName: 'file',
     maxSize: 1024,
     selectedFile: null,
     albumBucketName: 'routingstar-photo-album',
     bucketRegion: 'ap-northeast-2',
     IdentityPoolId: 'ap-northeast-2:65af3722-b840-4cce-8c5f-956fb7ed025e',
-    tmpImg:''
+    tmpImg: '',
   }),
   props: {
     place: {
-      type: Object
-    }
+      type: Object,
+    },
   },
   computed: {
-    ...mapGetters(['imgList'])
+    ...mapGetters(['imgList']),
   },
   methods: {
-    launchFilePicker(){
+    launchFilePicker() {
       this.$refs.file.click();
     },
     onFileSelected(place, fieldName, file) {
-      console.log(place.isThumbnail)
+      console.log(place.isThumbnail);
       // const { maxSize } = this
       // 파일 갱신되었는데 이미 썸네일인 경우도 고려해야함.
-      let imageFile = file[0]
-      if (file.length>0) {
+      let imageFile = file[0];
+      if (file.length > 0) {
         // let size = imageFile.size / maxSize / maxSize
-        this.imgList[place.createdOrder] = imageFile
-        
-        place.imageUpload = true
+        this.imgList[place.createdOrder] = imageFile;
+
+        place.imageUpload = true;
         // console.log(size)
 
         // Append file into FormData and turn file into image URL
-        let formData = new FormData()
-        let imageURL = URL.createObjectURL(imageFile)
+        let formData = new FormData();
+        let imageURL = URL.createObjectURL(imageFile);
 
-        formData.append(fieldName, imageFile)
-        place.placeImg = imageURL
+        formData.append(fieldName, imageFile);
+        place.placeImg = imageURL;
         // Emit the FormData and image URL to the parent component
-        this.$emit('input', { formData, imageURL })
+        this.$emit('input', { formData, imageURL });
         // 썸네일로 지정되어있던 상태에서 첨부파일이 갱신된다면
-        
+
         if (place.isThumbnail) {
-          this.tmpImg = imageURL
-          this.$store.state.images.thumbnailImage = imageURL
+          this.tmpImg = imageURL;
+          this.$store.state.images.thumbnailImage = imageURL;
         }
       }
     },
-    thumbnailSendToS3 (image) {
+    thumbnailSendToS3(image) {
       // console.log('ㅇㅇㅇ')
       const date = new Date().getTime();
       AWS.config.update({
         region: this.bucketRegion,
         credentials: new AWS.CognitoIdentityCredentials({
-        IdentityPoolId: this.IdentityPoolId,
-        })
+          IdentityPoolId: this.IdentityPoolId,
+        }),
       });
       // 썸네일 지정시 프론트에서 바로 업로드(리팩토링 필요)
       var s2 = new AWS.S3({
-        apiVersion: "2006-03-01",
-        params: { Bucket: this.albumBucketName }
+        apiVersion: '2006-03-01',
+        params: { Bucket: this.albumBucketName },
       });
-      s2.upload({
-        Key: `thumbnail/${date + image.name}`,
-        Body: image,
-        ContentType: image.type,
-        ACL: 'public-read'
-      }, (err, data) => {
-        if (err) {
-          console.log(err)
-          return alert("There was an error uploading your photo: ", err.message);
+      s2.upload(
+        {
+          Key: `thumbnail/${date + image.name}`,
+          Body: image,
+          ContentType: image.type,
+          ACL: 'public-read',
+        },
+        (err, data) => {
+          if (err) {
+            console.log(err);
+            return alert('There was an error uploading your photo: ', err.message);
+          }
+          this.$store.state.images.thumbnailImage = data.Location;
+          console.log('바꾼다잉');
+          this.tmpImg = data.Location;
+          // this.$emit('update-tumbnail-image', data.Location)
         }
-        this.$store.state.images.thumbnailImage = data.Location
-        console.log('바꾼다잉')
-        this.tmpImg = data.Location
-        // this.$emit('update-tumbnail-image', data.Location)
-      })
+      );
     },
     // emitImage(imageURL) {
     //   console.log(imageURL)
@@ -105,10 +116,10 @@ export default {
     // }
   },
   watch: {
-    tmpImg: function (imageURL) {
-      this.$emit('update-tumbnail-image', imageURL)
+    tmpImg: function(imageURL) {
+      this.$emit('update-tumbnail-image', imageURL);
       // this.emitImage(imageURL)
-    }
-  }
-}
+    },
+  },
+};
 </script>
