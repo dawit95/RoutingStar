a6a37f59a7859367c3b3f5c73da10e2e1f3593b8
hyunohenn <hyun.ohenn@gmail.com>
2021-08-16 14:28:10 +0900

    FE_0816: css 루트 등록 후반부 페이지
    Former-commit-id: 1082aeb66f585a05531734b7256e57aabbd0b045

--- frontend/src/components/search/SearchTagForm.vue
+++ frontend/src/components/search/SearchTagForm.vue
@@ -127,31 +127,5 @@ export default {
 </script>
 
 <style>
-/* .button {
-  width: 70px;
-  height: 25px;
-  background: #C1C8E4;
-  border-radius: 16px;
-  text-align: center;
-  cursor: pointer;
-  transition: all 0.3x ease-in-out;
-  filter: drop-shadow(0px 6px 6px rgba(0, 0, 0, 0.25));
-  margin-top: 6px;
-}
-.button span {
-  line-height: 10px;
-  font-size: 13px;
-  font-weight: bold;
-  color: #303C6C;
-}
-.chooseButton {
-  width: 70px;
-  height: 25px;
-  background: #B4DFE5;
-  border-radius: 16px;
-  text-align: center;
-  cursor: pointer;
-  transition: all 0.3x ease-in-out;
-  box-shadow: inset 0px 6px 6px rgba(0, 0, 0, 0.25);
-} */
+
 </style>
