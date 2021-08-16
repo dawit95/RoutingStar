1bdc449750078bad59449d72061835219d933d43
dawit95 <dawit0310@gmail.com>
2021-08-17 01:52:34 +0900

    FE_0817:add logout process & draggable more
    Former-commit-id: f1740683e24411d2d90bc28389b0ab573b6cb8b5 [formerly fb9f6cb8aad9233660c253e79589f249bbfb1f44]
    Former-commit-id: 76d8fe3c1fc3082d1fdd371174e879708676ab01

--- frontend/src/components/reviseothers/UpdateOthersMapPointForm.vue
+++ frontend/src/components/reviseothers/UpdateOthersMapPointForm.vue
@@ -1,44 +1,75 @@
 <template>
   <v-flex xs12 class="big-box rounded-lg">
-    <draggable class="card-box" @update="onUpdated">
-      <v-list outlined v-for="(place, idx) in places" :key="place.createdOrder" class="rounded-lg mb-2">
-
+    <draggable handle=".wehandler" class="card-box" @update="onUpdated">
+      <v-list
+        outlined
+        v-for="(place, idx) in places"
+        :key="place.createdOrder"
+        class="rounded-lg mb-2"
+      >
         <v-card flat class="d-flex">
           <v-card flat>
-            <v-icon large color="indigo" right style="cursor: pointer;" class=" ma-0 pa-0 drag-position">mdi-drag-horizontal-variant</v-icon>
-            <others-image-input v-model="avatar" :place="place" @update-tumbnail-image="updateThumbnailImage">
+            <v-icon
+              large
+              color="indigo"
+              right
+              style="cursor: pointer;"
+              class=" ma-0 pa-0 drag-position wehandler"
+              >mdi-drag-horizontal-variant</v-icon
+            >
+            <others-image-input
+              v-model="avatar"
+              :place="place"
+              @update-tumbnail-image="updateThumbnailImage"
+            >
               <div slot="activator">
-                <v-avatar size="140px" rounded v-ripple v-if="!place.placeImg" class="grey lighten-3 ml-1">
+                <v-avatar
+                  size="140px"
+                  rounded
+                  v-ripple
+                  v-if="!place.placeImg"
+                  class="grey lighten-3 ml-1"
+                >
                   <v-icon color="indigo" dark x-large>
                     <!-- 2A355D / 303c6c -->
                     mdi-plus
                   </v-icon>
                 </v-avatar>
                 <v-avatar size="140px" rounded v-ripple v-else class="lighten ml-1">
-                  <img :src="place.placeImg" alt="Image">
+                  <img :src="place.placeImg" alt="Image" />
                 </v-avatar>
               </div>
             </others-image-input>
           </v-card>
           <v-card flat class="d-flex flex-column">
-            <v-text-field v-model="place.content" @click="activePoint(place)" @mouseout="stopPoint(place)" 
-              label="간략한 설명" color="indigo" class="text-field-font" rows="1">
-                <v-icon right slot="prepend" color="grey">mdi-comment</v-icon>
-              </v-text-field>
-              <v-switch
+            <v-text-field
+              v-model="place.content"
+              @click="activePoint(place)"
+              @mouseout="stopPoint(place)"
+              label="간략한 설명"
+              color="indigo"
+              class="text-field-font"
+              rows="1"
+            >
+              <v-icon right slot="prepend" color="grey">mdi-comment</v-icon>
+            </v-text-field>
+            <v-switch
               class="switch-prop"
               id="thumbnail_switch"
               @click="refreshThumbnailBtn(place)"
-              :label="place.isThumbnail ? thumbnailLabel : '썸네일 등록하기!'" 
-              :disabled="(!place.isThumbnail && isthumbail) || !place.imageUpload" 
+              :label="place.isThumbnail ? thumbnailLabel : '썸네일 등록하기!'"
+              :disabled="(!place.isThumbnail && isthumbail) || !place.imageUpload"
               v-model="place.isThumbnail"
-              inset color="indigo darken-3"></v-switch>
+              inset
+              color="indigo darken-3"
+            ></v-switch>
           </v-card>
           <v-card flat class="d-flex flex-column">
-            <v-icon left style="cursor: pointer;" @click="removePoint(place.marker, idx)">mdi-close</v-icon>
+            <v-icon left style="cursor: pointer;" @click="removePoint(place.marker, idx)"
+              >mdi-close</v-icon
+            >
             <v-spacer></v-spacer>
           </v-card>
-
         </v-card>
       </v-list>
     </draggable>
@@ -46,12 +77,11 @@
 </template>
 
 <script>
-
-import { mapGetters, mapMutations, } from 'vuex'
-import draggable from 'vuedraggable'
-import { dragscroll } from 'vue-dragscroll'
-import AWS from 'aws-sdk'
-import OthersImageInput from '@/components/reviseothers/OthersImageInput.vue'
+import { mapGetters, mapMutations } from 'vuex';
+import draggable from 'vuedraggable';
+import { dragscroll } from 'vue-dragscroll';
+import AWS from 'aws-sdk';
+import OthersImageInput from '@/components/reviseothers/OthersImageInput.vue';
 
 export default {
   name: 'UpdateOthersMapPointForm',
@@ -61,16 +91,16 @@ export default {
   },
   props: {
     isthumbail: {
-      type: Boolean
+      type: Boolean,
     },
     listLength: {
-      type: Number
-    }
+      type: Number,
+    },
   },
   directives: {
-    dragscroll
+    dragscroll,
   },
-  data () {
+  data() {
     return {
       // 현재 썸네일이 골라진 상태인지 유무
       // thumbnailChecked: this.isthumbail,
@@ -80,7 +110,7 @@ export default {
       bucketRegion: 'ap-northeast-2',
       IdentityPoolId: 'ap-northeast-2:65af3722-b840-4cce-8c5f-956fb7ed025e',
       avatar: null,
-    }
+    };
   },
   computed: {
     ...mapGetters(['places', 'imgList', 'polyLine', 'thumbnailImage']),
@@ -88,7 +118,7 @@ export default {
   methods: {
     // 일단 데이터의 조작만 있고, 모든 데이터의 input이 완료된 이후에 백앤드와 통신하니
     // 일단은 mutations함수를 사용하였습니다.
-    ...mapMutations(['UPDATE_DRAGGERBLE_ITEMS','REFRESH_PLACES']),
+    ...mapMutations(['UPDATE_DRAGGERBLE_ITEMS', 'REFRESH_PLACES']),
     onUpdated(event) {
       this.UPDATE_DRAGGERBLE_ITEMS(event);
       this.refreshPolyline();
@@ -100,30 +130,30 @@ export default {
       place.marker.location.setAnimation(null);
     },
     removePoint(marker, idx) {
-      if ( idx != -1 ) {
+      if (idx != -1) {
         marker.location.setMap(null);
-        const oldItems = this.places
+        const oldItems = this.places;
         const newplaces = oldItems.filter((place) => {
           if (place.createdOrder === marker.createdOrder && place.isThumbnail) {
-            this.$emit('change-isthumbail')
+            this.$emit('change-isthumbail');
             // this.isthumbail = !this.isthumbail
-            place.isThumbnail = false
-            this.thumbnailLabel = ''
-            this.$store.state.images.thumbnailChecked = false
+            place.isThumbnail = false;
+            this.thumbnailLabel = '';
+            this.$store.state.images.thumbnailChecked = false;
           }
-          return place.createdOrder !== marker.createdOrder
-        })
-        this.REFRESH_PLACES(newplaces)
+          return place.createdOrder !== marker.createdOrder;
+        });
+        this.REFRESH_PLACES(newplaces);
         this.refreshPolyline();
       }
     },
     refreshPolyline() {
       const path = this.polyLine.getPath();
-      const places = this.places
-      console.log(places)
+      const places = this.places;
+      console.log(places);
       path.clear();
-      for( const point of places ) {
-        path.push( new window.google.maps.LatLng( point.lat, point.lng));
+      for (const point of places) {
+        path.push(new window.google.maps.LatLng(point.lat, point.lng));
       }
     },
     // refreshThumbnailBtn(place) {
@@ -170,51 +200,54 @@ export default {
     refreshThumbnailBtn(place) {
       // 썸네일인 경우 => 썸네일 취소
       if (this.isthumbail) {
-        this.$emit('change-isthumbail')
+        this.$emit('change-isthumbail');
         // this.isthumbail = !this.isthumbail
-        place.isThumbnail = false
-        this.thumbnailLabel = '썸네일 등록하기!'
-        this.$store.state.images.thumbnailChecked = false
-      // 썸네일이 아닌경우 => 첨부파일이 등록되어있다면 => 썸네일로 지정
+        place.isThumbnail = false;
+        this.thumbnailLabel = '썸네일 등록하기!';
+        this.$store.state.images.thumbnailChecked = false;
+        // 썸네일이 아닌경우 => 첨부파일이 등록되어있다면 => 썸네일로 지정
       } else if (this.isthumbail === false) {
-        this.$emit('change-isthumbail')
+        this.$emit('change-isthumbail');
         // this.isthumbail = !this.isthumbail
-        place.isThumbnail = true
-        this.thumbnailLabel = '썸네일 등록완료!'
+        place.isThumbnail = true;
+        this.thumbnailLabel = '썸네일 등록완료!';
 
-        const image = this.imgList[place.createdOrder]
+        const image = this.imgList[place.createdOrder];
         const date = new Date().getTime();
         AWS.config.update({
           region: this.bucketRegion,
           credentials: new AWS.CognitoIdentityCredentials({
-          IdentityPoolId: this.IdentityPoolId,
-          })
+            IdentityPoolId: this.IdentityPoolId,
+          }),
         });
         // 썸네일 지정시 프론트에서 바로 업로드(리팩토링 필요)
         var s3 = new AWS.S3({
-          apiVersion: "2006-03-01",
-          params: { Bucket: this.albumBucketName }
+          apiVersion: '2006-03-01',
+          params: { Bucket: this.albumBucketName },
         });
-        s3.upload({
-          Key: `thumbnail/${date + image.name}`,
-          Body: image,
-          ContentType: image.type,
-          ACL: 'public-read'
-        }, (err, data) => {
-          if (err) {
-            console.log(err)
-            return alert("There was an error uploading your photo: ", err.message);
+        s3.upload(
+          {
+            Key: `thumbnail/${date + image.name}`,
+            Body: image,
+            ContentType: image.type,
+            ACL: 'public-read',
+          },
+          (err, data) => {
+            if (err) {
+              console.log(err);
+              return alert('There was an error uploading your photo: ', err.message);
+            }
+            this.$store.state.images.thumbnailImage = data.Location;
+            this.$emit('update-tumbnail-image', data.Location);
           }
-          this.$store.state.images.thumbnailImage = data.Location
-          this.$emit('update-tumbnail-image', data.Location)
-        })
+        );
       }
     },
     updateThumbnailImage(imgUrl) {
-      this.$emit('update-tumbnail-image', imgUrl)
-    }
+      this.$emit('update-tumbnail-image', imgUrl);
+    },
   },
-}
+};
 </script>
 
 <style scoped>
@@ -223,14 +256,14 @@ export default {
   height: 330px;
   overflow: scroll;
   margin: 5px 15px 0 15px;
-  background-color: #2A355D;
+  background-color: #2a355d;
   overflow-x: hidden;
 }
 .big-box::-webkit-scrollbar {
   width: 15px;
 }
 .big-box::-webkit-scrollbar-thumb {
-  background-color: #2A355D;
+  background-color: #2a355d;
   background-clip: padding-box;
   border-radius: 10px;
   border: 2px solid transparent;
@@ -243,16 +276,16 @@ export default {
 .card-box {
   margin: 10px 10px 10px 10px;
 }
-input[type=file]::file-selector-button {
-  border: 2px solid #2A355D;
-  padding: .2em .4em;
-  border-radius: .2em;
-  background-color: #B4DFE5;
+input[type='file']::file-selector-button {
+  border: 2px solid #2a355d;
+  padding: 0.2em 0.4em;
+  border-radius: 0.2em;
+  background-color: #b4dfe5;
   transition: 1s;
 }
 
-input[type=file]::file-selector-button:hover {
-  background-color: #FBE8A6;
+input[type='file']::file-selector-button:hover {
+  background-color: #fbe8a6;
 }
 
 .switch-prop {
