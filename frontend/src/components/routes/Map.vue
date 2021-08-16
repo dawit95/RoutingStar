88b6a14edbd9674b949f42e5a3554613d029e65e
edgar <edkim3275@gmail.com>
2021-08-16 22:06:44 +0900

    FE_0816: Map 마커 축소, route detail 마커변경
    Former-commit-id: 453efd440e27e964b67a9513212ee7e580cf6a0b

--- frontend/src/components/routes/Map.vue
+++ frontend/src/components/routes/Map.vue
@@ -142,7 +142,7 @@ export default {
         position: latLng,
         map: this.map,
         animation: window.google.maps.Animation.DROP,
-        icon: 'https://routingstar-photo-album.s3.ap-northeast-2.amazonaws.com/assets/pin_sample(xxs).png'
+        icon: 'https://routingstar-photo-album.s3.ap-northeast-2.amazonaws.com/assets/pin_sample(xxxs).png'
       });
       // 마커 더블클릭시 삭제
       marker.addListener('dblclick', (e) => {
