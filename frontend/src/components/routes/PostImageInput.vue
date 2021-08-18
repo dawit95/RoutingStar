574e395acaf05bfcaebb817ced299a634dd3f7c8
edgar <edkim3275@gmail.com>
2021-08-18 10:40:40 +0900

    FE_0818: image accept 등록 후 확인용
    Former-commit-id: 7bfcc54b3c12988c10ef1a432d893d3059cc416c [formerly aa04bf38fe9a35e8ed52831a6a3afff99e3ee9dd]
    Former-commit-id: af40ffd6286d14482264356b21356c54f3cc8286

--- frontend/src/components/routes/PostImageInput.vue
+++ frontend/src/components/routes/PostImageInput.vue
@@ -14,6 +14,7 @@
     <input
       type="file"
       ref="file"
+      accept="image/*, .heic"
       :name="uploadFieldName"
       @change="onFileSelected(place, $event.target.name, $event.target.files)"
       style="display:none"
