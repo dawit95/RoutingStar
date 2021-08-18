b3bd03b516dbf9d18797a97dcac97f3912745b3c
dawit95 <dawit0310@gmail.com>
2021-08-18 20:49:20 +0900

    FE_0818: 로그인 갱신
    Former-commit-id: 0d08c36e61fd559ac178d2833e3e5a832a6e99b3 [formerly 180a8cb027a2280aef83a0b3f8a7d1fc66769d2c]
    Former-commit-id: c193dc57388ef396441cbb61d7d073a06aa14531

--- frontend/src/views/LoginProcessView.vue
+++ frontend/src/views/LoginProcessView.vue
@@ -3,24 +3,23 @@
 </template>
 
 <script>
-import axios from 'axios'
-import { mapActions, mapGetters } from 'vuex'
+import axios from 'axios';
+import { mapActions, mapGetters } from 'vuex';
 
 export default {
   name: 'LoginProcessView',
   computed: {
-    ...mapGetters(['jwt', 'getterbrowserToken'])
+    ...mapGetters(['jwt', 'getterbrowserToken']),
   },
   methods: {
-    ...mapActions(['sendBrowerToken'])
-  }, 
+    ...mapActions(['sendBrowerToken']),
+  },
   mounted() {
-    console.log('여기 로그인 프로세스 마운트')
+    console.log('여기 로그인 프로세스 마운트');
     // if (this.jwt[0].legth) {
     //   this.$router.push( {name: 'LoginProcessView'} )
     // }
-    this.$router.push( {name: 'HomeView'} )
-    
+    this.$router.push({ name: 'HomeView' });
   },
   // watch: {
   //   jwt: function(jwt) {
@@ -30,37 +29,40 @@ export default {
   //     }
   //   }
   // },
-  created () {
-    console.log("loginprocess is created!!!!!!!" + this.$route);
-  // 로그인 되 있는 사용자인지 / 처음으로 로그인 된 사용자인지 / 로그인 안된 사용자인지
-  if (this.jwt[0]) {
-    console.log('token이 이미 저장이 되어있음')
-    this.$store.dispatch('fetchLoginedToken', this.jwt)
-    this.$store.dispatch('fetchLoginedFeeds', this.jwt)
-  }
-  else if (this.$route.query.access && this.$route.query.refresh){
-    console.log('token을 지금 처음 저장함')
-    const token = 
-      {
+  created() {
+    console.log('loginprocess is created!!!!!!!' + this.$route);
+    // 로그인 되 있는 사용자인지 / 처음으로 로그인 된 사용자인지 / 로그인 안된 사용자인지
+    //  if (this.jwt[0]) {
+    //    console.log('token이 이미 저장이 되어있음')
+    //    this.$store.dispatch('fetchLoginedToken', this.jwt)
+    //    this.$store.dispatch('fetchLoginedFeeds', this.jwt)
+    //  }
+    //  else if (this.$route.query.access && this.$route.query.refresh){
+    if (this.$route.query.access && this.$route.query.refresh) {
+      console.log('token을 지금 처음 저장함');
+      const token = {
         access: this.$route.query.access,
-        refresh: this.$route.query.refresh
-      }
-      this.$store.dispatch('createUser', token)
-      this.$store.dispatch('createHome', token)
-    } 
-  else {
-      console.log('token이 없음')
+        refresh: this.$route.query.refresh,
+      };
+      this.$store.dispatch('createUser', token);
+      this.$store.dispatch('createHome', token);
+    } else {
+      console.log('token이 없음');
       // axios.get('http://i5a309.p.ssafy.io:8000/api/v1/routes')
-      axios.get('https://i5a309.p.ssafy.io/api/guest/routes')
-      .then((res) => {console.log(res)})
-      .catch((err) => {console.log(err)})
-  } 
+      axios
+        .get('https://i5a309.p.ssafy.io/api/guest/routes')
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
     // main page 들어오자마자 피드 정보들 받아오기
     // 참조: https://jasonwatmore.com/post/2020/07/23/vue-axios-http-get-request-examples
     // 현재 내가 로그인되있어서 user pk가 가지고 있다면
     // likes, routes_storage, routes에 요청을 보내야 필요한 정보를 모두 얻을 수 있음
     // store/home.js 생성 후 state 에 정보 저장
-
   },
   // watch: {
   //   getterbrowserToken: function() {
@@ -76,9 +78,7 @@ export default {
   //     this.sendBrowerToken(data)
   //   }
   // }
-}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
